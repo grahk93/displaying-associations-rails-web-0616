2cef6e2c086d57ebd3a2e380067f23ca143e7141
grahk93 <grace.juster@flatironschool.com>
2016-07-10 15:32:02 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,15 @@
 class CategoriesController < ApplicationController
+  def index 
+    @categories = Category.all 
+  end 
+  def new
+    @category = Category.new
+  end
+  def create
+    @catrgory = Category.create(name: params[:name])
+  end 
   def show
     @category = Category.find(params[:id])
   end
+ 
 end
